2e35056dd8c88b6cc9b0ccd1c394a56d00804206
Leo Reynal <leoreynal.pro@gmail.com>
2021-05-28 12:33:50 +0200

    try to fix failed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
+console.log("coco");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -31,5 +32,5 @@ document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   listenFocus();
-  NIMPORTEQUOI();
+  NIMPORRTQUOI();
 });
