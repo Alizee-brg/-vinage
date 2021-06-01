a110692d7bdae2e8513f005c01a6cbe691a9cc19
Alizée <76754044+Alizee-brg@users.noreply.github.com>
2021-06-01 17:53:59 +0200

    production graph

---
+++ app/javascript/channels/resultFilterType.js
@@ -0,0 +1,10 @@
+// const resultList = document.querySelectorAll('.result-list > li');
+
+// const btnRouge = document.getElementById('filter-rouge');
+
+
+// btnRouge.addEventListener("click", () => {
+//   resultList.forEach((element) => {
+//     console.log(element)
+//   }
+// })
