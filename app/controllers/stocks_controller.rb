67a52b41a63623326ccf2ca4a4f09398577feca3
Edouard Bernard <edouard.bernard@live.fr>
2021-06-02 12:20:56 +0200

    Camembert cellar show + quantités + affichage input quantité sur bottle show

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,32 @@
 class StocksController < ApplicationController
 
   def create
-    @stock = Stock.new(stocks_params)
-    @stock.user = current_user
+    
+    if Stock.find_by(bottle_id: params[:bottle_id]).present?
+      @stock = Stock.find_by(bottle_id: params[:bottle_id])
+      @stock.update(quantity: @stock.quantity + 1)
+    else 
+      @stock = Stock.new
+      @stock.cellar = current_user.cellar
+      @stock.bottle = Bottle.find(params[:bottle_id])
+      @stock.quantity = 1
+    end
+    
     if @stock.save!
-      redirect_to cellar_path
+      redirect_to cellars_show_path(@stock.cellar)
     end
   end
+
   def update
     @stock = Stock.find(params[:id])
     if @stock.update!(stock_params)
-      redirect_to cellar_path
+      redirect_to cellars_show_path(@stock.cellar)
     end
   end
 
   private
 
   def stocks_params
-    params.require(:stocks).permit(:quantity)
+    params.require(:stocks).permit(:bottle_id, :quantity)
   end
 end
