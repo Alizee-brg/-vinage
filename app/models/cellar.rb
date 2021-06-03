fe58d4bae9c8da6fb682a425713f4215c760b210
Alizée <76754044+Alizee-brg@users.noreply.github.com>
2021-06-03 17:39:47 +0200

    Yeah :sparkle:

--- app/models/cellar.rb
+++ app/models/cellar.rb
@@ -2,4 +2,10 @@ class Cellar < ApplicationRecord
   belongs_to :user
   has_many :stocks
   has_many :bottles, through: :stocks
+
+  def count_quantity
+    count = 0
+    self.stocks.each { |stock| stock.quantity > 0 ? count += stock.quantity : count += 0 }
+    count
+  end
 end
